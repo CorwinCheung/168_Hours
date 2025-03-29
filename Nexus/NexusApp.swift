67bc65f56793efff4ec7c28a21abc828718e36fb
Corwin <gmail@corwincheung.com>
2025-03-29 02:29:38 -0400

    Initial Commit

---
+++ Nexus/NexusApp.swift
@@ -0,0 +1,17 @@
+//
+//  NexusApp.swift
+//  Nexus
+//
+//  Created by Corwin Cheung on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct NexusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
